f4d6a0b09d4e131b9ed28f363b2c89d1f8260d0d
karthikeyanasha24 <ashakarthikeyan24@gmail.com>
2025-09-09 23:53:36 +0530

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
+    include: ['@stripe/stripe-js'], // Added this line
     exclude: ['lucide-react'],
   },
 });
