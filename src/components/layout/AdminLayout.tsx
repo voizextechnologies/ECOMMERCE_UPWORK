c2e0d40d2384a2a44478ce1a006c6dd9a189f6c7
karthikeyanasha24 <ashakarthikeyan24@gmail.com>
2025-09-12 18:47:02 +0530

    Updated AdminSidebar.tsx

--- src/components/layout/AdminLayout.tsx
+++ src/components/layout/AdminLayout.tsx
@@ -12,9 +12,6 @@ export function AdminLayout({ children }: AdminLayoutProps) {
     <div className="flex min-h-screen bg-gray-100">
       <AdminSidebar />
       <div className="flex-1 flex flex-col">
-        <AdminHeader />
-        <main className="flex-1 overflow-auto"> {/* Removed p-6 from here */}
-          <div className="bg-white rounded-lg shadow-md p-6 min-h-full">
             {children}
           </div>
         </main>
