18d516331b026050749e0b325494094887beb5ed
karthikeyanasha24 <ashakarthikeyan24@gmail.com>
2025-09-26 16:28:18 +0530

    Updated SellerCategoryDepartmentListPage.tsx

--- src/pages/SellerCategoryDepartmentListPage.tsx
+++ src/pages/SellerCategoryDepartmentListPage.tsx
@@ -1,4 +1,3 @@
-```typescript
 // src/pages/SellerCategoryDepartmentListPage.tsx
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
@@ -162,4 +161,3 @@ export function SellerCategoryDepartmentListPage() {
     </div>
   );
 }
-```
